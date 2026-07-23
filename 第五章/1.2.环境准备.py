64bd3b20e97ef305ecc125779a67493b600df5b0
Angienan <2489188158@qq.com>
2026-07-23 22:06:02 +0800

    数据分析.1

---
+++ 第五章/1.2.环境准备.py
@@ -0,0 +1,7 @@
+"""
+jupter Notebook: 是一个交互式的计算环境，支持多种编程语言，可以方便地进行代码编写、运行和结果可视化。
+pandas: 是一个强大的数据处理和分析工具，提供了数据结构和数据操作功能。,核心:DataFrame Series
+                    DataFrame   :    表格数据结构
+                    Series      :    一维数据结构,就像DataFrame 单独的一列
+"""
+
