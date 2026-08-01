c2f92a81cb0c67d4e6b15a4bcf280f7dd810507f
Angienan <2489188158@qq.com>
2026-08-01 14:19:33 +0800

    FastAPI

---
+++ 第七章/1.1.web.py
@@ -0,0 +1,8 @@
+"""
+web : 全球广域网,万维网,www
+三大核心部分 : 前端程序,服务端程序,数据库
+前端程序 : HTML,CSS,JavaScript 三部分组成
+
+服务端 : 可以基于Python中的Django,Flask,FastAPI来进行开发
+
+"""
---
+++ 第七章/1_2_FastAPI.py
@@ -0,0 +1,27 @@
+"""
+FastAPI: 是一个现代,快速,高性能的web框架,用于基于标准的Python类型提示构建的API接口服务
+
+"""
+from fastapi import FastAPI
+
+#创建实例
+app = FastAPI()
+
+#定义API接口,返回值是API接口返回的数据
+@app.get("/") #接口访问路径是/,请求方式是GET
+def root():
+    return {"message": "Hello World"}
+
+#定义API接口
+@app.get("/users")
+def get_users():
+    return [
+        {"username": "john", "email": ""},
+        {"username": "alice", "email": ""},
+        {"username": "bob", "email": ""}
+    ]
+
+#启动服务,uvicorn: python中的轻量级web服务器
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app,host="127.0.0.1",port=8000)
